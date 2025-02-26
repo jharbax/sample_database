797c4fefef28bdb4dd7af3950c8bc1ca4316eb58
jharbax <jharbax@gmail.com>
2025-02-26 15:34:13 -0300

    Added RETURNING id in Inserts

--- sql/inserts/INSERT account.sql
+++ sql/inserts/INSERT account.sql
@@ -2,4 +2,5 @@
 INSERT INTO "sample_docker".public."account"
 ("id", "user_client_id", "user_manager_id", "balance", "locked_value", "created", "last_update")
 VALUES
-(DEFAULT, 0, 0, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
+(DEFAULT, 0, 0, DEFAULT, DEFAULT, DEFAULT, DEFAULT)
+RETURNING "id";
--- sql/inserts/INSERT user.sql
+++ sql/inserts/INSERT user.sql
@@ -2,4 +2,5 @@
 INSERT INTO "sample_docker".public."user"
 ("id", "role_id", "name", "login", "hash", "created", "last_update")
 VALUES
-(DEFAULT, 0, 'user name', 'login', 'hash', DEFAULT, DEFAULT);
+(DEFAULT, 0, 'user name', 'login', 'hash', DEFAULT, DEFAULT)
+RETURNING "id";
