450a73650d486db9541e51d78fcd91ba4e591921
jharbax <jharbax@gmail.com>
2025-03-17 12:32:07 -0300

    Create Select bank_statement and adjust CREATE TABLE

--- sql/tables/CREATE TABLE bank_statement.sql
+++ sql/tables/CREATE TABLE bank_statement.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS "sample_docker".public."bank_statement"
 	"id" SERIAL PRIMARY KEY,
 	"account_id" INTEGER NOT NULL,
     "withdrawal_account" INTEGER NULL,
+    "target_account" INTEGER NULL,
     "operation_id" INTEGER NOT NULL,
     "value" NUMERIC(12, 2) NOT NULL,
 	"balance_before" NUMERIC(12, 2) NOT NULL,
@@ -11,6 +12,6 @@ CREATE TABLE IF NOT EXISTS "sample_docker".public."bank_statement"
 	"dt_operation" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT "fk_account" FOREIGN KEY ("account_id") REFERENCES "sample_docker".public."account"("id"),
     CONSTRAINT "fk_withdrawal_account" FOREIGN KEY ("withdrawal_account") REFERENCES "sample_docker".public."account"("id"),
+    CONSTRAINT "fk_target_account" FOREIGN KEY ("target_account") REFERENCES "sample_docker".public."account"("id"),
 	CONSTRAINT "fk_operation" FOREIGN KEY ("operation_id") REFERENCES "sample_docker".public."bank_operation"("id")
 );
-
